e58ecdd646fd814dce1b443a57ef2a8526863e1d
Firas Moosvi <firas@moosvi.com>
2020-01-29 18:46:06 -0800

    Add initial version of script that takes 5s to run

---
+++ longscript.py
@@ -0,0 +1,15 @@
+# This is a script that will take a long time to run
+
+import numpy as np
+from datetime import datetime
+
+startTime = datetime.now()
+
+data = np.arange(0,100000)
+
+for i in range(0,100000):
+    data*=data
+
+endTime = datetime.now()
+
+print('The script took {0:.2f} s to execute'.format((endTime - startTime).total_seconds()))
